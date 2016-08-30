10e7e7d15f7ee01cfec6c10e7323865584564b59
Nazar Kozak <nzrkzk@gmail.com>
2016-08-30 23:53:18 +0300

    init

---
+++ updateMultidexManifest.js
@@ -0,0 +1,15 @@
+module.exports = function(ctx) {
+    var fs = ctx.requireCordovaModule('fs'),
+        path = ctx.requireCordovaModule('path'),
+        xml = ctx.requireCordovaModule('cordova-common').xmlHelpers;
+
+    var manifestPath = path.join(ctx.opts.projectRoot, 'platforms/android/AndroidManifest.xml');
+    var doc = xml.parseElementtreeSync(manifestPath);
+    if (doc.getroot().tag !== 'manifest') {
+        throw new Error(manifestPath + ' has incorrect root node name (expected "manifest")');
+    }
+
+    doc.getroot().find('./application').attrib['android:name'] = 'android.support.multidex.MultiDexApplication';
+
+    fs.writeFileSync(manifestPath, doc.write({indent: 4}), 'utf-8');
+};
